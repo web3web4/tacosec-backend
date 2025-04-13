c98034ca689bee8190cb58529fb77a7e842d8c4d
hasan <samar.qaddour@ramaaz.dev>
2025-04-13 13:32:50 +0300

    fix/new

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  
+  app.enableCors();
+  await app.init();
+  
+  return server;
 }
-bootstrap();
+
+// This is important for Vercel
+const handler = bootstrap();
+export default handler;

9d7030684b0f458a007a76fdd7aae8a2e25cbdae
hasan <samar.qaddour@ramaaz.dev>
2025-04-13 14:21:10 +0300

    fix/13

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-let app;
 
 async function bootstrap() {
-  if (!app) {
-    const expressApp = express();
-    const nestApp = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-      {
-        logger: ['error', 'warn']
-      }
-    );
-    
-    nestApp.enableCors();
-    await nestApp.init();
-    app = expressApp;
-  }
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   return app;
 }
 
-export default bootstrap;
+let cachedApp;
+
+async function handler(request, response) {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+    await cachedApp.init();
+  }
+  
+  cachedApp.getHttpAdapter().getInstance()(request, response);
+}
+
+export default handler;

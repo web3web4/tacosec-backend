fe58cb1e42712ae256b43d2662338f401ed16fd7
hasan <samar.qaddour@ramaaz.dev>
2025-04-13 14:27:57 +0300

    fix/add-node-env

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let app;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+  }
   return app;
 }
 
-let cachedApp;
-
-async function handler(request, response) {
-  if (!cachedApp) {
-    cachedApp = await bootstrap();
-    await cachedApp.init();
-  }
-  
-  cachedApp.getHttpAdapter().getInstance()(request, response);
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then(app => {
+    app.listen(3000).then(() => {
+      console.log('Server running on http://localhost:3000');
+    });
+  });
 }
+
+export const handler = async (request, response) => {
+  const app = await bootstrap();
+  const httpAdapter = app.getHttpAdapter();
+  return httpAdapter.getInstance()(request, response);
+};
 
 export default handler;

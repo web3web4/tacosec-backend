76207e45be3ab4ea16519fb1dc7d51b4e0744412
hasan <samar.qaddour@ramaaz.dev>
2025-04-20 16:47:22 +0300

    fix/link-atlas

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/taco'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   exports: [MongooseModule],
 })
